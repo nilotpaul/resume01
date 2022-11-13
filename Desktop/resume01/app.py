6a4ca33735e3c053998fc907f247a6d0bea7629c
nilotpaul <112468300+nilotpaul@users.noreply.github.com>
2022-11-13 22:52:11 +0530

    Update app.py

--- Desktop/resume01/app.py
+++ Desktop/resume01/app.py
@@ -3,20 +3,17 @@ from pathlib import Path
 import streamlit as st
 from PIL import Image
 
-
-# --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "CV.pdf"
 profile_pic = current_dir / "assets" / "profile-pic.jpg"
 
 
-# --- GENERAL SETTINGS ---
 PAGE_TITLE = "Resume | Nilotpaul Nandi"
 PAGE_ICON = ":wave:"
 NAME = "Nilotpaul Nandi"
 DESCRIPTION = """
-Idiot, bc, 3rd class coder
+Idiot, 3rd class coder
 """
 EMAIL = "nilotpaul.nandi@gmail.com"
 
@@ -26,7 +23,6 @@ EMAIL = "nilotpaul.nandi@gmail.com"
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
 
-# --- LOAD CSS, PDF & PROFIL PIC ---
 with open(css_file) as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 with open(resume_file, "rb") as pdf_file:
@@ -34,7 +30,6 @@ with open(resume_file, "rb") as pdf_file:
 profile_pic = Image.open(profile_pic)
 
 
-# --- HERO SECTION ---
 col1, col2 = st.columns(2, gap="small")
 with col1:
     st.image(profile_pic, width=230)
